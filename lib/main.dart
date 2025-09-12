f7bf2b2f21ab49922138db3caef762413eaed5b5
Gabriel <gabrielvasco906@gmail.com>
2025-09-12 10:10:40 -0300

    feat: Configure linter and formatter settings; set up MultiProvider in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wubba_lubba/app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [],
+      child: const MyApp(),
+    ),
+  );
 }
